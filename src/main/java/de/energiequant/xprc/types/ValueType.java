1ca42a82544bd760c77e735e3a673d103bfe3c42
Daniel Neugebauer <dneuge@energiequant.de>
2025-01-03 18:41:33 +0100

    ValueType: resolve for deserialization

--- src/main/java/de/energiequant/xprc/types/ValueType.java
+++ src/main/java/de/energiequant/xprc/types/ValueType.java
@@ -1,5 +1,7 @@
 package de.energiequant.xprc.types;
 
+import java.util.Optional;
+
 public interface ValueType<T> {
     ValueType<Integer> INTEGER = new IntegerValueType();
     ValueType<Float> FLOAT = new FloatValueType();
@@ -13,4 +15,29 @@ public interface ValueType<T> {
     String serialize(T value);
 
     T deserialize(String s);
+
+    static Optional<ValueType<?>> resolve(String encodedTypeName) {
+        switch (encodedTypeName) {
+            case "int":
+                return Optional.of(INTEGER);
+
+            case "float":
+                return Optional.of(FLOAT);
+
+            case "double":
+                return Optional.of(DOUBLE);
+
+            case "int[]":
+                return Optional.of(INTEGER_ARRAY);
+
+            case "float[]":
+                return Optional.of(FLOAT_ARRAY);
+
+            case "blob":
+                return Optional.of(BLOB);
+
+            default:
+                return Optional.empty();
+        }
+    }
 }
