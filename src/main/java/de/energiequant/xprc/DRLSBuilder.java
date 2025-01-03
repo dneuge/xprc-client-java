4d2726018718e899a20e1e828d8731dec22204c6
Daniel Neugebauer <dneuge@energiequant.de>
2025-01-03 18:52:05 +0100

    DRLSBuilder: command parametrization

--- src/main/java/de/energiequant/xprc/DRLSBuilder.java
+++ src/main/java/de/energiequant/xprc/DRLSBuilder.java
@@ -1,10 +1,24 @@
 package de.energiequant.xprc;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Stream;
+
 public class DRLSBuilder extends Command.Builder<DRLSBuilder, DRLSMessage, DRLSDecoder> {
     // FIXME: draft/WIP
-    
+
+    private static final String READWRITE_OPTION = "rwCheck";
+
     public enum ReadWriteCheck {
-        SIMULATOR, SERVER, SESSION;
+        SIMULATOR("simulator"),
+        SERVER("server"),
+        SESSION("session");
+
+        private final String serialization;
+
+        ReadWriteCheck(String serialization) {
+            this.serialization = serialization;
+        }
     }
 
     public DRLSBuilder() {
@@ -12,14 +26,33 @@ public class DRLSBuilder extends Command.Builder<DRLSBuilder, DRLSMessage, DRLSD
     }
 
     public DRLSBuilder withReadWriteCheck(ReadWriteCheck check) {
-        return this;
+        return setOption(READWRITE_OPTION, check.serialization);
     }
 
-    public DRLSBuilder forDataRefs(String... dataRefs) {
-        return this;
+    public DRLSBuilder forDataRefs(DataRef<?>... dataRefs) {
+        return forDataRefs(Arrays.stream(dataRefs));
     }
 
-    public DRLSBuilder forDataRefs(DataRef... dataRefs) {
+    public DRLSBuilder forDataRefs(Collection<DataRef<?>> dataRefs) {
+        return forDataRefs(dataRefs.stream());
+    }
+
+    public DRLSBuilder forDataRefs(Stream<DataRef<?>> dataRefs) {
+        return forDataRefsNamed(dataRefs.map(DataRef::getName));
+    }
+
+    public DRLSBuilder forDataRefsNamed(String... dataRefs) {
+        return forDataRefsNamed(Arrays.stream(dataRefs));
+    }
+
+    public DRLSBuilder forDataRefsNamed(Collection<String> dataRefs) {
+        return forDataRefsNamed(dataRefs.stream());
+    }
+
+    public DRLSBuilder forDataRefsNamed(Stream<String> dataRefs) {
+        // TODO: catch empty strings
+        // TODO: deduplicate
+        dataRefs.forEach(this::addParameter);
         return this;
     }
 }
