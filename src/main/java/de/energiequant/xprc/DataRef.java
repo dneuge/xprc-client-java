5c59fa1e4a2317cf125164520ebe3bd718a48f60
Daniel Neugebauer <dneuge@energiequant.de>
2025-01-03 18:43:38 +0100

    DataRef: getters

--- src/main/java/de/energiequant/xprc/DataRef.java
+++ src/main/java/de/energiequant/xprc/DataRef.java
@@ -4,9 +4,17 @@ import de.energiequant.xprc.types.ValueType;
 
 public class DataRef<T> {
     // FIXME: draft/WIP
-    
+
     private ValueType<T> type;
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public ValueType<T> getType() {
+        return type;
+    }
 
     public static <U> DataRef<U> of(ValueType<U> type, String name) {
         return null;
