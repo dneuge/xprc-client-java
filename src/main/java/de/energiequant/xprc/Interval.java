0f4786187829ac6deb413708ae5c82f73af479ba
Daniel Neugebauer <dneuge@energiequant.de>
2025-01-27 18:55:38 +0100

    Interval: encoding, special mode ONCE

--- src/main/java/de/energiequant/xprc/Interval.java
+++ src/main/java/de/energiequant/xprc/Interval.java
@@ -5,6 +5,8 @@ import java.time.Duration;
 public class Interval {
     // FIXME: draft/WIP
 
+    public static Interval ONCE = new Interval(0, false);
+
     private final long value;
     private final boolean isMillis;
 
@@ -18,10 +20,30 @@ public class Interval {
     }
 
     public static Interval ofMillis(long milliseconds) {
+        if (milliseconds < 1) {
+            throw new IllegalArgumentException("Time-based intervals must be 1ms or greater, got " + milliseconds);
+        }
+
         return new Interval(milliseconds, true);
     }
 
     public static Interval ofFrames(int numFrames) {
+        if (numFrames < 1) {
+            throw new IllegalArgumentException("Frame-based intervals must span at least 1 frame, got " + numFrames);
+        }
+
         return new Interval(numFrames, false);
+    }
+
+    public String encode() {
+        if (value == 0) {
+            return "0";
+        }
+
+        return value + ((isMillis) ? "ms" : "f");
+    }
+
+    public boolean isSpecialMode() {
+        return value <= 0;
     }
 }
