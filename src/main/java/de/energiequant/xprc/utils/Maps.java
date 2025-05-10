a3eb5d2eafe62845c3c10012155aa7daeb00c02f
Daniel Neugebauer <dneuge@energiequant.de>
2025-05-10 12:41:46 +0200

    Maps: JavaDoc

--- src/main/java/de/energiequant/xprc/utils/Maps.java
+++ src/main/java/de/energiequant/xprc/utils/Maps.java
@@ -6,36 +6,95 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * Helper methods for {@link Map}s.
+ */
 public class Maps {
     private Maps() {
         // utility class; hide constructor
     }
 
+    /**
+     * Creates a new unmodifiable {@link HashMap} holding the given entries.
+     *
+     * @param entries map entries
+     * @param <K>     key type
+     * @param <V>     value type
+     * @return unmodifiable {@link HashMap} holding the given entries
+     * @see #entry(Object, Object)
+     */
     @SafeVarargs
     public static <K, V> Map<K, V> createUnmodifiableHashMap(Map.Entry<K, V>... entries) {
         return Collections.unmodifiableMap(createHashMap(entries));
     }
 
+    /**
+     * Creates a new unmodifiable {@link LinkedHashMap} holding the given entries, maintaining the same order.
+     *
+     * @param entries map entries
+     * @param <K>     key type
+     * @param <V>     value type
+     * @return unmodifiable {@link LinkedHashMap} holding the given entries in order
+     * @see #entry(Object, Object)
+     */
     @SafeVarargs
     public static <K, V> Map<K, V> createUnmodifiableLinkedHashMap(Map.Entry<K, V>... entries) {
         return Collections.unmodifiableMap(createLinkedHashMap(entries));
     }
 
+    /**
+     * Creates a new unmodifiable {@link EnumMap} holding the given entries.
+     *
+     * @param clazz   enum type to be used as key
+     * @param entries map entries
+     * @param <K>     key type
+     * @param <V>     value type
+     * @return unmodifiable {@link EnumMap} holding the given entries
+     * @see #entry(Object, Object)
+     */
     @SafeVarargs
     public static <K extends Enum<K>, V> Map<K, V> createUnmodifiableEnumMap(Class<K> clazz, Map.Entry<K, V>... entries) {
         return Collections.unmodifiableMap(createEnumMap(clazz, entries));
     }
 
+    /**
+     * Creates a new {@link HashMap} initially holding the given entries.
+     *
+     * @param entries initial map entries
+     * @param <K>     key type
+     * @param <V>     value type
+     * @return {@link HashMap} holding the given entries
+     * @see #entry(Object, Object)
+     */
     @SafeVarargs
     public static <K, V> HashMap<K, V> createHashMap(Map.Entry<K, V>... entries) {
         return putAll(new HashMap<>(), entries);
     }
 
+    /**
+     * Creates a new {@link LinkedHashMap} initially holding the given entries, maintaining the same order.
+     *
+     * @param entries initial map entries
+     * @param <K>     key type
+     * @param <V>     value type
+     * @return {@link LinkedHashMap} holding the given entries in order
+     * @see #entry(Object, Object)
+     */
     @SafeVarargs
     public static <K, V> LinkedHashMap<K, V> createLinkedHashMap(Map.Entry<K, V>... entries) {
         return putAll(new LinkedHashMap<>(), entries);
     }
 
+    /**
+     * Creates a new {@link EnumMap} initially holding the given entries.
+     *
+     * @param clazz   enum type to be used as key
+     * @param entries initial map entries
+     * @param <K>     key type
+     * @param <V>     value type
+     * @return {@link EnumMap} holding the given entries
+     * @see #entry(Object, Object)
+     */
     @SafeVarargs
     public static <K extends Enum<K>, V> EnumMap<K, V> createEnumMap(Class<K> clazz, Map.Entry<K, V>... entries) {
         return putAll(new EnumMap<>(clazz), entries);
@@ -54,6 +113,15 @@ public class Maps {
         return dest;
     }
 
+    /**
+     * Creates a {@link Map.Entry} to be used in other helper methods provided by {@link Maps}.
+     *
+     * @param key   entry key
+     * @param value entry value
+     * @param <K>   key type
+     * @param <V>   value type
+     * @return {@link Map.Entry} to be used in other helper methods provided by {@link Maps}
+     */
     public static <K, V> Map.Entry<K, V> entry(K key, V value) {
         return new Map.Entry<K, V>() {
             @Override
